25532f1f2734bbcba10b71e2e9044c4f218a1627
fenK <mihha1818@gmail.com>
2019-08-30 10:41:16 +0600

    new

--- index.js
+++ index.js
@@ -60,38 +60,38 @@ vk.updates.hear(/^\/para/i, async (context) => {
     }
 
     if (hour >= 0 && hour < 8) {
-        context.send(file[dat.getMonth()][dat.getDate()])
+        context.send("1:" +file[dat.getMonth()][dat.getDate()])
     } else if (hour >= 8 && hour <= 9) {
         if (hour == 9 && min >= 30) {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["3"])
+            context.send("3:" +file[dat.getMonth() + 1][dat.getDate()]["3"])
         } else {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["2"])
+            context.send("2:" +file[dat.getMonth() + 1][dat.getDate()]["2"])
 
         }
     } else if (hour >= 10 && hour <= 11) {
         if ((hour == 11 && min >= 10)) {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["4"])
+            context.send("4:" +file[dat.getMonth() + 1][dat.getDate()]["4"])
         } else {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["3"])
+            context.send("3:" +file[dat.getMonth() + 1][dat.getDate()]["3"])
 
         }
     } else if (hour >= 12 && hour <= 13) {
         if (hour == 13 && min >= 10) {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["5"])
+            context.send("5:"+file[dat.getMonth() + 1][dat.getDate()]["5"])
         } else {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["4"])
+            context.send("4:" +file[dat.getMonth() + 1][dat.getDate()]["4"])
 
         }
     } else if (hour >= 14 && hour <= 15) {
         if (hour == 15 && min >= 10) {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["6"])
+            context.send("6:" +file[dat.getMonth() + 1][dat.getDate()]["6"])
         } else {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["5"])
+            context.send("5:" +file[dat.getMonth() + 1][dat.getDate()]["5"])
 
         }
     } else if (hour >= 18) {
         if (hour == 18 && min < 10) {
-            context.send(file[dat.getMonth() + 1][dat.getDate()]["6"])
+            context.send("6:" +file[dat.getMonth() + 1][dat.getDate()]["6"])
         } else {
             context.send("Поздно уже для пар то")
 

56ffdb2fc158dc58479a09258c74dbe6b6207e51
fenK <mihha1818@gmail.com>
2019-09-02 10:38:20 +0600

    stable

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const vk = new VK({
 });
 var rasp = "https://sun9-3.userapi.com/c846122/v846122093/d6776/NqM_20zkAbM.jpg"
 
+
+var TelegramBot = require('node-telegram-bot-api'); // Устанавливаем токен, который выдавал нам бот
+var token = '917044014:AAEWZIEZOgjGmYnXjscYRMYFda259a88Tx8'; // Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию 
+var bot = new TelegramBot(token, { polling: true });
+
+
+
 const { SessionManager } = require('@vk-io/session');
 const { SceneManager, StepScene } = require('@vk-io/scenes');
 const sessionManager = new SessionManager();
@@ -39,8 +46,8 @@ vk.updates.hear('/cat', async (context) => {
 });
 
 vk.updates.hear(['/time', '/date'], async (context) => {
-    var dat=new Date();
-    context.send(dat.getHours()+":"+dat.getMinutes()+":"+dat.getSeconds()+"\n"+dat.getDate()+"."+(dat.getMonth()+1)+"."+dat.getFullYear());
+    var dat = new Date();
+    context.send(dat.getHours() + ":" + dat.getMinutes() + ":" + dat.getSeconds() + "\n" + dat.getDate() + "." + (dat.getMonth() + 1) + "." + dat.getFullYear());
 });
 vk.updates.hear(/^\/para/i, async (context) => {
     var dat = new Date();
@@ -57,62 +64,64 @@ vk.updates.hear(/^\/para/i, async (context) => {
         var min = dat.getMinutes();
     }
     var para;
+    bot.sendMessage("518054807", "hour:" + hour + "\n" + "min:" + min);
+
 
     if (hour >= 0 && hour < 8) {
-       para=1;
+        para = 1;
     } else if (hour >= 8 && hour <= 9) {
         if (hour == 9 && min >= 40) {
-           para=3;
+            para = 3;
         } else {
-            para=2;
+            para = 2;
 
         }
     } else if (hour >= 10 && hour <= 11) {
         if ((hour == 11 && min >= 40)) {
-           para=4;
+            para = 4;
         } else {
-           para=3;
+            para = 3;
 
         }
     } else if (hour >= 12 && hour <= 13) {
         if (hour == 13 && min >= 30) {
-           para=5;
+            para = 5;
         } else {
-           para=4;
+            para = 4;
 
         }
     } else if (hour >= 14 && hour <= 15) {
         if (hour >= 15 && min >= 10) {
-            para=6;
+            para = 6;
         } else {
-            para=5;
+            para = 5;
 
         }
-    } else if (hour >= 16 && hour <= 18) {
+    } else if (hour == 16) {
         if (hour >= 16 && min >= 50) {
             para = "S";
         } else {
             para = 6;
         }
-        }else if (hour >= 18){
+    } else if (hour >= 17) {
         if (hour <= 18 && min >= 20) {
             para = "S";
         } else {
             para = "Poz";
-        }  
         }
-           
+    }
 
-        
-    
-    
-    if (para=="Poz"){
+
+    bot.sendMessage("518054807", para);
+
+
+    if (para == "Poz") {
         context.send("Поздно для пар уже");
-    }else if (para == "S"){
+    } else if (para == "S") {
         context.send("Это последняя пара");
-    } else if (file[dat.getMonth() + 1][dat.getDate()][String(para)].toUpperCase() != "NULL" && file[dat.getMonth() + 1][dat.getDate()][String(para)].toUpperCase() !="Щ")
-    context.send(para + ":" + file[dat.getMonth() + 1][dat.getDate()][String(para)])
-    else{
+    } else if (file[dat.getMonth() + 1][dat.getDate()][String(para)].toUpperCase() != "NULL" && file[dat.getMonth() + 1][dat.getDate()][String(para)].toUpperCase() != "Щ") {
+        context.send(para + ":" + file[dat.getMonth() + 1][dat.getDate()][String(para)])
+    } else {
         context.send("Пары нет/рассписание не установлено")
     }
 });
@@ -122,30 +131,30 @@ vk.updates.hear(/^\/reverse (.+)/i, async (context) => {
         context.$match[1].split('').reverse().join('')
     );
 });
-vk.updates.hear(/^\/para (.+)/i, async (context) => {
+/*vk.updates.hear(/^\/para (.+)/i, async (context) => {
     await context.send(
         context.$match[1].split('').reverse().join('')
     );
-});
+});*/
 
 vk.updates.hear(/^\/ras/i, async (context) => {
     var com = context.text.split(" ");
-    
+
     file = JSON.parse(fs.readFileSync('schedule.json', 'utf-8'))
     switch (com.length) {
         case 1:
-            context.send(getRasp(file[new Date().getMonth()+1][new Date().getDate()]))
+            context.send(getRasp(file[new Date().getMonth() + 1][new Date().getDate()]))
             break;
 
         case 2: if (com[1].toUpperCase() == "NEXT") {
             var dat = new Date();
             dat.setDate(dat.getDate() + 1)
-            context.send(getRasp(file[dat.getMonth()+1][dat.getDate()]))
+            context.send(getRasp(file[dat.getMonth() + 1][dat.getDate()]))
         } else if (com[1].toUpperCase() == "IMG") {
             context.sendPhoto(rasp)
         }
         else
-            context.send(getRasp(file[new Date().getMonth()+1][com[1]])); break;
+            context.send(getRasp(file[new Date().getMonth() + 1][com[1]])); break;
         case 3: context.send(getRasp(file[com[2]][com[1]])); break;
         default: context.send("Неверное количество параметров");
     }
@@ -176,7 +185,7 @@ sceneManager.addScene(new StepScene('upr', [
         if (com[0] == "/upr") {
             switch (com.length) {
                 case 1:
-                    month = new Date().getMonth()+1;
+                    month = new Date().getMonth() + 1;
                     day = new Date().getDate();
                     break;
 
@@ -184,29 +193,29 @@ sceneManager.addScene(new StepScene('upr', [
                     var dat = new Date();
                     dat.setDate(dat.getDate() + 1)
 
-                    month = dat.getMonth()+1;
+                    month = dat.getMonth() + 1;
                     day = dat.getDate();
                 } else {
 
-                    month = new Date().getMonth()+1;
+                    month = new Date().getMonth() + 1;
                     day = com[1];
                 }
                     break;
                 case 3:
                     day = com[1];
                     month = com[2];
-                    
+
                     break;
 
             }
-           // context.send("day:"+day+"\n"+"month:"+month)
-         
-            if (inInterval(month, "m") && inInterval(day, "d")){
+            //context.send("day:"+day+"\n"+"month:"+month)
+
+            if (inInterval(month, "m") && inInterval(day, "d")) {
                 return context.send('Введите данные');
             }
-            
-            else{
-                 context.scene.leave();
+
+            else {
+                context.scene.leave();
             }
         }
 
@@ -219,19 +228,19 @@ sceneManager.addScene(new StepScene('upr', [
         // console.log(month)
         // console.log("\n")
         var iz = context.text.split(" ");
-        
-        if (inInterval(month, "m") && inInterval(day, "d")){
-        for (var i = 1; i <= 6; i++) {
-            if (i <= iz.length) {
-                file[month][day][i.toString()] = iz[i - 1]
-            } else
-                file[month][day][i.toString()] = "Null"
+
+        if (inInterval(month, "m") && inInterval(day, "d")) {
+            for (var i = 1; i <= 6; i++) {
+                if (i <= iz.length) {
+                    file[month][day][i.toString()] = iz[i - 1]
+                } else
+                    file[month][day][i.toString()] = "Null"
+            }
+            fs.writeFileSync('schedule.json', JSON.stringify(file, null, 2));
+            file = JSON.parse(fs.readFileSync('schedule.json', 'utf-8'))
+            context.send("Рассписание установлено");
+            await context.scene.leave();
         }
-        fs.writeFileSync('schedule.json', JSON.stringify(file, null, 2));
-        file = JSON.parse(fs.readFileSync('schedule.json', 'utf-8'))
-        context.send("Рассписание установлено");
-        await context.scene.leave();
-    }
     }
 ]));
 
@@ -266,8 +275,8 @@ function getRasp(n) {
     return (str);
 
 }
-function inInterval(value,p) {
-    if ((value >= 1 && value <= 12 && p == "m") || (value >= 1 && value <= 30 && p == "d" ))  {
+function inInterval(value, p) {
+    if ((value >= 1 && value <= 12 && p == "m") || (value >= 1 && value <= 31 && p == "d")) {
         return true;
     } else {
         return false;

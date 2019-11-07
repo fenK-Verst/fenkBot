b773db9d8d583bf514e0d7a12a40af7bb5aa2b17
fenK <mihha1818@gmail.com>
2019-11-07 17:45:21 +0600

    123

--- index.js
+++ index.js
@@ -135,19 +135,10 @@ vk.updates.hear(/^\/reverse (.+)/i, async (context) => {
 });
 
 vk.updates.hear(/^\/ras/i, async (context) => {
-<<<<<<< HEAD
-   
+
     var com = context.text.split(" ");
     let date = new Date();
     date.setHours(0, 0, 0, 0);
-=======
-    if (context.senderId == 262742265){
-        return
-    }
-    var com = context.text.split(" ");
-    fs.appendFileSync("f.txt", JSON.stringify(context));
-    file = JSON.parse(fs.readFileSync('schedule.json', 'utf-8'))
->>>>>>> 99de882588b7b22afa8fd6c4b522d1ff712d75e5
     switch (com.length) {
         case 1:
             getRasp(date).then((value) => {
@@ -198,11 +189,6 @@ vk.updates.hear(/^\/imp/i, async (context) => {
 
 });
 vk.updates.hear(/^\/simp/i, async (context) => {
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 99de882588b7b22afa8fd6c4b522d1ff712d75e5
     await context.scene.enter('OVS');
 
 });
@@ -278,18 +264,18 @@ sceneManager.addScene(new StepScene('upr', [
                     return val;
                 }
             })
-            for (var i=0;i<6;i++){
-                if(!temp[i]){
-                    temp[i]="Null"
+            for (var i = 0; i < 6; i++) {
+                if (!temp[i]) {
+                    temp[i] = "Null"
                 }
             }
 
 
             let date = new Date();
             date.setHours(0, 0, 0, 0);
-            date.setFullYear(date.getFullYear(), month, day-1)
-            
-            
+            date.setFullYear(date.getFullYear(), month, day - 1)
+
+
             db.run(`UPDATE LESSONS SET l1=?,l2=?,l3=?,l4=?,l5=?,l6=? WHERE date=? `, temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], date, function (err) {
                 if (err) {
                     console.log(err.message);
@@ -349,21 +335,12 @@ vk.updates.on('message', sceneManager.middleware);
 vk.updates.on('message', sceneManager.middlewareIntercept);
 
 vk.updates.hear(/^\/upr/i, async (context) => {
-<<<<<<< HEAD
     if (context.senderId == 161830362 || context.senderId == 259251175 || context.senderId == 503131193) {
         await context.scene.enter('upr');
     } else {
         context.send("Тоби суда нельзя")
     }
 
-=======
-    if (context.senderId == 161830362 || context.senderId == 259251175 || context.senderId == 503131193){
-        await context.scene.enter('upr');
-    }else{
-        context.send("Тоби суда нельзя")
-    }
-   
->>>>>>> 99de882588b7b22afa8fd6c4b522d1ff712d75e5
 });
 
 
@@ -373,7 +350,6 @@ vk.updates.start().catch(console.error);
 
 
 
-<<<<<<< HEAD
 function getRasp(date) {
     return new Promise((resolve, reject) => {
         db.all('SELECT l1,l2,l3,l4,l5,l6,date FROM LESSONS WHERE DATE=?', date, function (err, rows) {
@@ -388,19 +364,12 @@ function getRasp(date) {
                 if (rows[0]["l" + i] != "Null")
                     temp += (i + ":" + rows[0]["l" + i] + "\n")
             }
-=======
-function getRasp(n, month, day) {
-    var str = "Рассписание на " + day + "." + month + "\n";
-    var ss = "";
-    for (key in n) {
->>>>>>> 99de882588b7b22afa8fd6c4b522d1ff712d75e5
 
             if (temp == "") {
                 if (date.getDay() == 0)
                     resolve("Воскресенье, нет пар")
                 resolve("На этот день еще нет рассписания");
 
-<<<<<<< HEAD
             }
 
 
@@ -423,14 +392,6 @@ function getRasp(n, month, day) {
          return ("На этот день еще нет рассписания")
      //return (str + ss);*/
     return (date.toString())
-=======
-            ss += key + ":" + n[key] + "\n"
-        }
-    }
-    if (ss == "")
-        return ("На этот день еще нет рассписания")
-    return (str + ss);
->>>>>>> 99de882588b7b22afa8fd6c4b522d1ff712d75e5
 
 }
 function inInterval(value, p) {

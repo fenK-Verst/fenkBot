051aec1fa7c1a311d0fb456e395e88593da2b85a
fenk <mixail.syursin@gmail.com>
2020-09-21 12:26:41 +0600

    w

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,8 @@
-import {Lessons} from "./Bot";
+import parseXlsx from 'excel';
+import {SchoolDay ,Lessons} from "../src/Bot";
 import SqDatabase from "./Db";
-
-let defaultDate = new Date();
-
+const fs = require('fs');
+const db = new SqDatabase();
 // @ts-ignore
 Date.prototype.getWeek = function () {
     let onejan = new Date(this.getFullYear(), 0, 1);
@@ -15,84 +15,82 @@ Date.prototype.getSqlite = function () {
     if (month.toString().length == 1) month = "0" + String(month);
     return `${this.getFullYear()}-${month}-${this.getDate()}`;
 };
+parseXlsx('outer/schedule.xlsx').then((parsed) => {
+   const data = [];
+   parsed.forEach(e => {
+       const arr = e.filter((d,i) => {
+           if (i > 13){
+               return !!d;
+           }
+           return true;
+       });
+      data.push(arr);
+   });
+   let index = 0;
+   const da = [];
+   while (data[index][0].length){
+       // console.log(data[index]);
+       const lessons = [];
+       const d = data[index];
+       const date = new Date(Date.parse(d[0]));
+       console.log(date);
+       lessons.push(date);
+        for (let i=1;i<d.length-1;i+=2){
+                let str = ``;
+                if (d[i].length){
+                    str+=d[i]+"\n";
+                }
+                str+=d[i+1];
+                lessons.push(str);
+        }
 
-const lessonsArray: Lessons[] = [
-    {
-        l1: ``,
-        l2: `Облачные вычисления`,
-        l3: `Охрана труда`,
-        l4: `Методы защиты информации`,
-        l5: `Физ-ра`,
-        l6: ``,
-    },
-    {
-        l1: ``,
-        l2: `Охрана трудая`,
-        l3: `Моделирование`,
-        l4: `Методы защиты информации`,
-        l5: `WEB ПиИТ`,
-        l6: ``,
-    },
-    {
-        l1: ``,
-        l2: ``,
-        l3: `Экономика`,
-        l4: `Методы защиты информации`,
-        l5: `WEB ПиИТ`,
-        l6: `Игры`,
-    },
-    {
-        l1: `Моделирование`,
-        l2: `Экономика`,
-        l3: `Облачные вычисления`,
-        l4: `Основы предпринимательской деятельности`,
-        l5: ``,
-        l6: ``,
-    },
-    {
-        l1: ``,
-        l2: `Игры/Охрана труда`,
-        l3: `Облачные вычисления`,
-        l4: `Основы предпринимательской деятельности`,
-        l5: `Моделирование/WEB ПиИт`,
-        l6: ``,
-    },
-];
-const db = new SqDatabase();
+       index++;
+       da.push(lessons);
+   }
 
-for (let i = 0; i < 150; i++) {
-    defaultDate = new Date(defaultDate.setDate(defaultDate.getDate() + 1));
-    // @ts-ignore
-    const date = defaultDate.getSqlite();
-    let ls: Lessons | undefined | string = lessonsArray[defaultDate.getDay() - 1];
-    // console.log(defaultDate.getSqlite() + ` `+ JSON.stringify(ls));
-    const lessons = JSON.stringify(ls);
-    db.all('SELECT * FROM lessons WHERE date=?', [date])
-        .then((rows) => {
-
-            let sql;
-            if (rows.length > 0) {
-                sql = `UPDATE lessons
-                       SET lessons=?
-                       WHERE date = ?`;
-            } else {
-                sql = `INSERT INTO lessons(lessons, date)
-                       VALUES (?, ?)`;
-            }
-            // console.log(sql, rows);
-            return db
-                .run(sql, [lessons, date])
-                .catch((err: any) => {
-                    throw err;
-                });
-
-        })
-        .catch(e => {
-            console.log(e.message);
-        })
-        .then(() => {
-            // console.log("Рассписание установлено");
-        })
+   // console.log(da);
+   da.forEach(e => {
+       let date = e[0],
+           l1=e[1],
+           l2=e[2],
+           l3=e[3],
+           l4=e[4],
+           l5=e[5],
+           l6=e[6];
 
 
-}
+       const lessonsRaw = {l1,l2,l3,l4,l5,l6},
+            lessons = JSON.stringify(lessonsRaw);
+
+
+       date.setHours(0, 0, 0, 0);
+
+       // @ts-ignore
+       date = date.getSqlite();
+       db
+           .all('SELECT * FROM lessons WHERE date=?', [date])
+           .then((rows) => {
+
+               let sql;
+               if (rows.length > 0) {
+                   sql = `UPDATE lessons SET lessons=? WHERE date=?`;
+               } else {
+                   sql = `INSERT INTO lessons(lessons,date) VALUES (?,?)`;
+               }
+               return db
+                   .run(sql, [lessons, date])
+                   .catch((err: any) => {
+                       throw err;
+                   });
+
+           })
+           .catch(e => {
+               console.log(e.message);
+           })
+           .then(() => {
+               console.log("Рассписание установлено");
+           })
+   })
+}).catch(e => {
+    console.error(e);
+});
